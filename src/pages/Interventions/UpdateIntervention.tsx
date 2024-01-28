e2b66f3a449fbefb93df46c03602e5712710ea52
Chiheb Moussi <chihebmoussi@gmail.com>
2024-01-28 23:04:16 +0100

    update

--- src/pages/Interventions/UpdateIntervention.tsx
+++ src/pages/Interventions/UpdateIntervention.tsx
@@ -11,13 +11,6 @@ const UpdateIntervention = () => {
 
     const {id} = useParams();
 
-
-    const detail_interventions: string[] = [
-        'Problème four',
-        'Problème machine de pause',
-        'Problème machine laser',
-    ];
-    
     const navigate = useNavigate();
     const [type_interventions, setTypeInterventions] = useState<TypeIntervention[]>([]);
     const [lines, setLines] = useState<Line[]>([]);
@@ -133,36 +126,15 @@ console.log(type_intervention);
                             <label className="mb-3 block text-black dark:text-white">
                                 Détail d'intervention *
                             </label>
-                            <div className="relative z-20 bg-white dark:bg-form-input">
-                                <select 
-                                    className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input"
+                            <input
+                                    type="text"
+                                    className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
                                     value={detail_intervention}
                                     onChange={(e) => {
                                         setDetailIntervention(e.target.value)
                                         setError('')
                                     }}
-                                >
-                                    {detail_interventions.map(d => <option key={d} value={d}>{d}</option>)}
-                                </select>
-                                <span className="absolute top-1/2 right-4 z-10 -translate-y-1/2">
-                                <svg
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <g opacity="0.8">
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                                        fill="#637381"
-                                    ></path>
-                                    </g>
-                                </svg>
-                                </span>
-                            </div>
+                                />
                             </div>)}
                             <div>
                                 <label className="mb-3 block text-black dark:text-white">
